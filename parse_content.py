efef7921587c1b49de412bd5c9ceaae748b5a661
Yuto Takano <moa17stock@gmail.com>
2021-07-15 23:36:16 +0100

    Support images written using relative links

--- parse_content.py
+++ parse_content.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import shutil
 from glob import glob
 import subprocess
 import yaml
@@ -18,13 +19,31 @@ def main():
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
-    # get all files with .md extension within the input dir
-    recipes = [y for x in os.walk(args.input_dir) for y in glob(os.path.join(x[0], "*.md"))]
+    page_bundles = parse_markdown(args.input_dir, args.output_dir)
+    move_images(args.input_dir, page_bundles)
+    print("Done.")
 
-    for recipe in recipes:
-        # skip unrelated files
-        if "LICENSE" in recipe or "README" in recipe:
+def parse_markdown(input_dir: str, output_dir: str) -> list[str]:
+    """
+    Parses all markdown files in the input directory and generates the output
+    files. Returns the written output directories.
+    """
+    # get all files within the input dir
+    all_markdown = [y for x in os.walk(input_dir) for y in glob(os.path.join(x[0], "*.md"))]
+
+    # list to put directories of generated page bundles (for use in image copying)
+    page_bundles = []
+
+    for file_path in all_markdown:
+        if not os.path.isfile(file_path):
             continue
+
+        # skip unrelated files
+        if "LICENSE" in file_path or "README" in file_path:
+            continue
+
+        print("Parsing " + file_path + "... ", end="")
+        recipe = file_path
 
         front_matter = {}
         front_matter.setdefault("layout", "recipe")
@@ -36,7 +55,7 @@ def main():
         # get the latest edit date from the git commit log
         relative_path = os.path.join(os.path.basename(meal), os.path.basename(difficulty), os.path.basename(recipe))
         git_command = ["git", "log", "-1", "--pretty=format:%cI", "--follow", "--", relative_path]
-        date = subprocess.check_output(git_command, cwd=args.input_dir).decode("utf-8")
+        date = subprocess.check_output(git_command, cwd=input_dir).decode("utf-8")
         front_matter.setdefault("publishdate", date)
 
         # read the existing front matter (if any) and contents
@@ -52,15 +71,50 @@ def main():
         if existing_front_matter:
             front_matter = front_matter | existing_front_matter
 
-        # write everything to a Page Bundle (see Hugo docs)
+        print(front_matter["title"])
+
+        # Write everything to a file
         basename = os.path.splitext(os.path.basename(recipe))[0]
-        if not os.path.exists(os.path.join(args.output_dir, basename)):
-            os.makedirs(os.path.join(args.output_dir, basename))
-        with open(os.path.join(args.output_dir, basename, "index.md"), "w+", encoding="utf-8") as f:
+        page_bundle = os.path.join(output_dir, basename)
+        if not os.path.exists(page_bundle):
+            os.makedirs(page_bundle)
+        page_bundles.append(page_bundle)
+        with open(os.path.join(page_bundle, "index.md"), "w+", encoding="utf-8") as f:
             f.write("---\n")
             f.writelines(yaml.dump(front_matter))
             f.write("---\n\n")
             f.writelines(contents)
+
+    return page_bundles
+
+def move_images(input_dir: str, page_bundles: list[str]) -> None:
+    """
+    Get all images and copy them into all page bundles. This is hugely
+    inefficient but I've thought about it for a few days and there is no clean
+    solution for image loading that is also contributor-friendly.
+
+    1. Copy all images into each page (they can be referenced relatively, but duplication)
+    2. Put all images in /static when parsing (relative URL won't work)
+    3. Put all images in /static in the cookbook (very clunky, unintuitive as hell)
+
+    Duplication is a problem with number 1, but since GitHub Page offers us
+    1GB for free, I decided the trade-off was most beneficial with 1 than the
+    other two.
+    """
+    all_files = [y for x in os.walk(input_dir) for y in glob(os.path.join(x[0], "**"))]
+
+    for file_path in all_files:
+        if not os.path.isfile(file_path):
+            continue
+
+        # filter only images
+        if file_path[-4:] not in [".png", ".jpg", ".gif"] and file_path[-5:] not in [".jpeg", ".webp"]:
+            continue
+
+        print("Copying " + file_path + "... ", end="")
+        for page_bundle in page_bundles:
+            shutil.copyfile(file_path, os.path.join(page_bundle, os.path.basename(file_path)))
+        print("Copied")
 
 def split_contents(contents: list[str]) -> (dict, str, str, list[str]):
     """
