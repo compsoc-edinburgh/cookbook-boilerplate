3e1474cc30231307cbc0399756b83f67ed6f87b8
Yuto Takano <moa17stock@gmail.com>
2021-11-21 06:22:31 +0000

    Make the two gigantic functions into modular tiny functions

--- parse_content.py
+++ parse_content.py
@@ -19,78 +19,102 @@ def main():
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
-    page_bundles = parse_generate_markdown(args.input_dir, args.output_dir)
-    move_images(args.input_dir, page_bundles)
+    parsed_contents: list[tuple[dict, list[str]]] = parse_all_markdown_files(args.input_dir)
+    page_bundle_names = [write_to_page_bundle(args.output_dir, *parsed_content) for parsed_content in parsed_contents]
+    move_images(args.input_dir, page_bundle_names)
     print("Done.")
 
-def parse_generate_markdown(input_dir: str, output_dir: str) -> list[str]:
+def get_all_markdown_files(input_dir: str) -> list[str]:
     """
-    Parses all markdown files in the input directory and generates the output
-    files. Returns the written output directories.
+    Get all markdown files within the input directory.
     """
     # get all files within the input dir
     all_markdown = [y for x in os.walk(input_dir) for y in glob(os.path.join(x[0], "*.md"))]
 
-    # list to put directories of generated page bundles (for use in image copying)
-    page_bundles = []
+    return [
+        file_path
+        for file_path
+        in all_markdown
+        if (os.path.isfile(file_path) and
+            "LICENSE" not in file_path and
+            "README" not in file_path)
+    ]
 
-    for file_path in all_markdown:
-        if not os.path.isfile(file_path):
-            continue
+def directory_structure_to_front_matter(file_path: str) -> dict[str, str]:
+    """
+    Converts the directory structure of a recipe into a front matter.
+    """
+    # Make sure the path is well-formed and normalised
+    path_to_recipe = os.path.normpath(file_path)
 
-        # skip unrelated files
-        if "LICENSE" in file_path or "README" in file_path:
-            continue
+    # Unpack the directory structure into variable names
+    *_, meal, difficulty, recipe_filename = path_to_recipe.split(os.sep)
 
+    # Set some front matter using the extracted data
+    return {
+        "layout": "recipe",
+        "difficulties": difficulty,
+        "meals": meal,
+        "originalfilename": recipe_filename,
+        "originalpath": os.path.join(meal, difficulty, recipe_filename)
+    }
+
+def git_date_to_front_matter(directory: str, rel_file_path: str) -> dict[str, str]:
+    git_command = ["git", "log", "-1", "--pretty=format:%cI", "--follow", "--", rel_file_path]
+    date = subprocess.check_output(git_command, cwd=directory).decode("utf-8")
+
+    return {
+        "publishdate": date
+	}
+
+def parse_all_markdown_files(input_dir: str) -> tuple[dict, list[str]]:
+    """
+    Parses all markdown files in the input directory and generates the output
+    files. Returns the written output directories.
+    """
+    parsed_contents = []
+
+    for file_path in get_all_markdown_files(input_dir):
         print("Parsing " + file_path + "... ", end="", flush=True)
-        recipe = file_path
 
         front_matter = {}
-        recipe_filename = os.path.basename(recipe)
-        up_to_difficulty = os.path.dirname(recipe)
-        difficulty = os.path.basename(up_to_difficulty)
-        up_to_meal = os.path.dirname(up_to_difficulty)
-        meal = os.path.basename(up_to_meal)
+        front_matter |= directory_structure_to_front_matter(file_path)
 
-        front_matter.setdefault("layout", "recipe")
-        front_matter.setdefault("difficulties", difficulty)
-        front_matter.setdefault("meals", meal)
+        # Get the latest edit date from the git commit log, add that in the
+        # front matter as the publishdate.
+        relative_path = front_matter["originalpath"]
+        front_matter |= git_date_to_front_matter(input_dir, relative_path)
 
-        # get the latest edit date from the git commit log
-        relative_path = os.path.join(meal, difficulty, recipe_filename)
-        git_command = ["git", "log", "-1", "--pretty=format:%cI", "--follow", "--", relative_path]
-        date = subprocess.check_output(git_command, cwd=input_dir).decode("utf-8")
-        front_matter.setdefault("publishdate", date)
-        front_matter.setdefault("originalpath", "/".join([meal, difficulty, recipe_filename]))
-
-        # read the existing front matter (if any) and contents
-        with open(recipe, "r", encoding="utf-8") as f:
+        # Read the existing front matter (if any) and contents
+        with open(file_path, "r", encoding="utf-8") as f:
             contents = f.readlines()
-        existing_front_matter, name, preview_image, contents = split_contents(contents)
 
-        front_matter.setdefault("title", name)
-        if preview_image is not None:
-            front_matter.setdefault("previewimage", preview_image)
-
-        # merge the front matters (3.9+)
-        if existing_front_matter:
-            front_matter = front_matter | existing_front_matter
+        content_front_matter, contents = parse_front_matter_and_content(contents)
+        front_matter |= content_front_matter
 
         print(front_matter["title"])
 
-        # Write everything to a file
-        basename = os.path.splitext(os.path.basename(recipe))[0]
-        page_bundle = os.path.join(output_dir, basename)
-        if not os.path.exists(page_bundle):
-            os.makedirs(page_bundle)
-        page_bundles.append(page_bundle)
-        with open(os.path.join(page_bundle, "index.md"), "w+", encoding="utf-8") as f:
-            f.write("---\n")
-            f.writelines(yaml.dump(front_matter))
-            f.write("---\n\n")
-            f.writelines(contents)
+        parsed_contents.append((front_matter, contents))
 
-    return page_bundles
+    return parsed_contents
+
+def write_to_page_bundle(output_dir: str, front_matter: dict, content: list[str]) -> str:
+    """
+    Write everything to a new file called index.md, within a folder
+    titled with the recipe filename. This creates a Hugo page bundle, so
+    we can put images within the same folder and reference them relatively.
+    """
+    basename = os.path.splitext(front_matter["originalfilename"])[0]
+    page_bundle_name = os.path.join(output_dir, basename)
+    if not os.path.exists(page_bundle_name):
+        os.makedirs(page_bundle_name)
+    with open(os.path.join(page_bundle_name, "index.md"), "w+", encoding="utf-8") as f:
+        f.write("---\n")
+        f.writelines(yaml.dump(front_matter))
+        f.write("---\n\n")
+        f.writelines(content)
+
+    return page_bundle_name
 
 def move_images(input_dir: str, page_bundles: list[str]) -> None:
     """
@@ -121,47 +145,51 @@ def move_images(input_dir: str, page_bundles: list[str]) -> None:
             shutil.copyfile(file_path, os.path.join(page_bundle, os.path.basename(file_path)))
         print("Copied")
 
-def split_contents(contents: list[str]) -> tuple[dict, str, str, list[str]]:
+def parse_front_matter_and_content(contents: list[str]) -> tuple[dict[str, str], list[str]]:
     """
-    Split a list of all lines in a file into:
-      front matter dictionary
-      name of recipe or <unnamed>
-      url for the first image (only supports ![]() style), None if not found
+    Parse a list of all lines in a file into:
+      front matter (merged result of existing ones and title + preview image)
       all markdown lines in the content apart from the recipe name
     """
-    front_matter = []
-    recipe_name = "<unnamed>"
-    image_url = None
-    meaningful_stuff = []
+    front_matter = {
+        "title": "<unnamed>"
+    }
+    front_matter_lines = []
+    markdown_content = []
+
+    # Emulate a FSM to parse the front matter
     currently_fm = False
     for line in contents:
         if line == "---\n":
             currently_fm = not currently_fm
             continue
 
-        if line[:2] == "# " and recipe_name == "<unnamed>":
-            recipe_name = line[2:].strip()
+        if line[:2] == "# " and front_matter["title"] == "<unnamed>":
+            front_matter["title"] = line[2:].strip()
             continue
 
         result = re.match("^!\[(.+)\]\((.+)\)", line)
-        if result is not None and image_url is None:
+        if result is not None and "previewimage" not in front_matter:
             # Take the second group
             image_url = result.group(2)
             # If there is a title ![](blah "title"), this removes it
-            image_url = image_url.split(" ")[0]
+            front_matter["previewimage"] = image_url.split(" ")[0]
 
         if currently_fm:
-            front_matter.append(line)
+            front_matter_lines.append(line)
         else:
-            meaningful_stuff.append(line)
+            markdown_content.append(line)
 
     try:
-        parsed = yaml.safe_load("\n".join(front_matter))
+        # PyYAML will return None if the YAML is simply not there (zero lines)
+        front_matter |= yaml.safe_load("\n".join(front_matter_lines)) or {}
     except Exception as e:
-        parsed = {}
+        # If there was an error parsing the YAML, there is a case the front
+        # matter isn't really a front matter (like a table). In which case,
+        # treat them as normal meaningful markdown content.
         meaningful_stuff = contents
 
-    return (parsed, recipe_name, image_url, meaningful_stuff)
+    return (front_matter, markdown_content)
 
 if __name__ == "__main__":
     main()
